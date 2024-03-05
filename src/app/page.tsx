f5d64fcd30d8a26cd750e5f44b8da919d3553748
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-03-05 12:15:05 +0100

    Adding first convex mutations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import {
   SignedOut,
   SignInButton,
   SignOutButton,
-  useSession,
 } from "@clerk/nextjs";
-import Image from "next/image";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-  const session = useSession();
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -25,6 +26,20 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
+      <Button
+        onClick={() => {
+          createFile({
+            name: "Hello world",
+          });
+        }}
+      >
+        Click me
+      </Button>
     </main>
   );
 }
