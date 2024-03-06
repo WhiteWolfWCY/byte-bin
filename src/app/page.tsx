dfd5886eded4a9347622d545f77effefab42aa71
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-03-06 15:18:08 +0100

    Uploading file logic, popup polishing, validations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,9 @@ import { api } from "../../convex/_generated/api";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   title: z.string().min(1).max(200),
@@ -43,26 +46,59 @@ const formSchema = z.object({
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { toast } = useToast();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
-      file: undefined
+      file: undefined,
     },
   });
 
   const fileRef = form.register("file");
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-    console.log(values.file)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) return;
+    const postUrl = await generateUploadUrl();
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": values.file[0].type },
+      body: values.file[0],
+    });
+    const { storageId } = await result.json();
+
+    try {
+      await createFile({
+        name: values.title,
+        fileId: storageId,
+        orgId,
+      });
+
+      form.reset();
+      setIsDialogOpen(false);
+      toast({
+        variant: "success",
+        title: "File Uploaded",
+        description: "Now you can share it with others!",
+      });
+    } catch (err) {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong",
+        description: "Your file could not be uploaded. Try again.",
+      });
+    }
   }
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
@@ -72,19 +108,15 @@ export default function Home() {
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold">Your files</h1>
 
-        <Dialog>
+        <Dialog
+          open={isDialogOpen}
+          onOpenChange={(isOpen) => {
+            setIsDialogOpen(isOpen);
+            form.reset();
+          }}
+        >
           <DialogTrigger asChild>
-            <Button
-              onClick={() => {
-                if (!orgId) return;
-                createFile({
-                  name: "Hello world",
-                  orgId,
-                });
-              }}
-            >
-              Upload file
-            </Button>
+            <Button onClick={() => {}}>Upload file</Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
@@ -94,40 +126,45 @@ export default function Home() {
               </DialogDescription>
             </DialogHeader>
             <div>
-            <Form {...form}>
-                  <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8"
-                  >
-                    <FormField
-                      control={form.control}
-                      name="title"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Title</FormLabel>
-                          <FormControl>
-                            <Input placeholder="Your file title" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="file"
-                      render={() => (
-                        <FormItem>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-8"
+                >
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Your file title" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="file"
+                    render={() => (
+                      <FormItem>
                         <FormLabel>File</FormLabel>
                         <FormControl>
                           <Input type="file" {...fileRef} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                      )}
-                    />
-                    <Button type="submit">Submit</Button>
-                  </form>
-                </Form>
+                    )}
+                  />
+                  <Button type="submit" disabled={form.formState.isSubmitting} className="flex gap-1">
+                    Submit
+                    {form.formState.isSubmitting && (
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    )}
+                  </Button>
+                </form>
+              </Form>
             </div>
           </DialogContent>
         </Dialog>
