1211ead7a84ecfea91e6f39713015ac3d9ac901a
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-03-08 11:00:02 +0100

    Default theme fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
         <ConvexClientProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
