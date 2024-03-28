af7b7ef94125182ec01035e92b29a0f5486ae206
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-03-28 21:19:44 +0100

    Setting metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from "next"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function constructMetadata({
+  title = 'ByteBin - store and share your files!',
+  description = 'ByteBin is a cloud storage for storing your files. You can create organizations and add people to them so that you can quickly share assets.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@mateusz_mww',
+    },
+    icons,
+    metadataBase: new URL('https://byte-bin-eight.vercel.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
+
